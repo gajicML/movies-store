0a9a9ded4b8724155a8de54c2855d65504dd5d6e
unknown <mla.gajic@gmail.com>
2020-01-23 00:39:37 +0100

    Creating searchInput component

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,23 +1,21 @@
 import React from "react";
-import { Button } from "react-bulma-components";
+import { Container, Button } from "react-bulma-components";
 
 const SearchInput = ({ value, onChange, onClick }) => {
   return (
-    <div className="row">
-      <div className="input-field col s6 offset-s3">
-        <input
-          autoFocus
-          value={value}
-          onChange={onChange}
-          type="text"
-          className="center white-text"
-          style={{ fontSize: 21 }}
-        />
-        <Button onClick={onClick} className="is-primary">
-          Search
-        </Button>
-      </div>
-    </div>
+    <Container>
+      <input
+        autoFocus
+        value={value}
+        onChange={onChange}
+        type="text"
+        className="center white-text"
+        style={{ fontSize: 21 }}
+      />
+      <Button onClick={onClick} className="is-primary">
+        Search
+      </Button>
+    </Container>
   );
 };
 
