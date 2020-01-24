c150e138f7e22e1c5795079e342b1be439370d61
unknown <mla.gajic@gmail.com>
2020-01-24 01:25:53 +0100

    moviesAction - handle top movies

--- src/store/actions/moviesActions.js
+++ src/store/actions/moviesActions.js
@@ -1,8 +1,7 @@
 import {
   FETCH_MOVIE_PENDING,
   FETCH_MOVIE_SUCCESS,
-  FETCH_MOVIE_ERROR,
-  FETCH_TOP_MOVIES
+  FETCH_MOVIE_ERROR
 } from "./type";
 
 const MOVIE_DB_API_KEY = "abc6ac3532a6ce1c8313d30773b32578";
@@ -22,15 +21,16 @@ const fetchMovieError = error => ({
   payload: error
 });
 
-const fetchTopMovies = () => ({
-  type: FETCH_TOP_MOVIES
-});
-
-export const fetchMovie = name => {
+export const fetchMovie = arg => {
+  let url = "";
+  if (typeof arg === "number") {
+    url = `${MOVIE_DB_BASE_URL}/movie/top_rated?api_key=${MOVIE_DB_API_KEY}&language=en-US&page=${arg}`;
+  } else {
+    url = `${MOVIE_DB_BASE_URL}/search/movie?api_key=${MOVIE_DB_API_KEY}&language=en-US&query=${arg}`;
+  }
   return async dispatch => {
     dispatch(fetchMoviePending());
     try {
-      const url = `${MOVIE_DB_BASE_URL}/search/movie?api_key=${MOVIE_DB_API_KEY}&language=en-US&query=${name}`;
       const response = await fetch(url);
       const result = await response.json(response);
 
